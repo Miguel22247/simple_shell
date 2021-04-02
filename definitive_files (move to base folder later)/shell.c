4aad3096d3dd7ebb55ef2125bb2b9d8f3a105b95
Miguel22247 <minipachru@gmail.com>
2021-04-02 15:17:07 -0300

    fixed thing

--- definitive_files (move to base folder later)/shell.c
+++ definitive_files (move to base folder later)/shell.c
@@ -11,7 +11,7 @@ int main(int ac __attribute__((unused)), char **av __attribute__((unused)))
 	{
 		int_mode = isatty(STDIN_FILENO);
 		if (int_mode == 1)
-			write(STDOUT_FILENO, “~$ “, 13);
+			write(STDOUT_FILENO, PROMPT, 13);
 	}
 	
 /* The code continues below */
