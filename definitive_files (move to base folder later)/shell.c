be5503b259e0c5fd571f9a8e80fd4bbdd8c020c2
Miguel22247 <minipachru@gmail.com>
2021-04-02 14:36:47 -0300

    test

--- definitive_files (move to base folder later)/shell.c
+++ definitive_files (move to base folder later)/shell.c
@@ -1,9 +1,35 @@
 #include "shell.h"
+
+/**
+ *  _strcat - concatenates two strings
+ * @src: source string = s2 in main
+ * @dest: destination string = s1 in main
+ * Return: pointer to dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+int count = 0, j;
 /*
-*
+ * stores the length of the destination string on the count variable
+*/
+while (dest[count] != '\0')
+{
+++count;
+}
+
+/*
+* start_shell
 *
 */
 
 void start_shell() {
-	
+	char *current_path[], *buffer, *prompt;
+
+	while (1)
+	{
+		current_path = getcwd(buffer, BUFSIZE);
+		prompt = _strcat(current_path, PROMPT)
+		_putchar(prompt);
+	}
 }
