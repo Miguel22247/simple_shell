d5344e85b9ff0f81abe6cc4a3b390d9ba31cec7b
Miguel22247 <minipachru@gmail.com>
2021-04-07 15:09:54 -0300

    fixed compiler errors

--- shell.c
+++ shell.c
@@ -9,14 +9,14 @@ int shell(void)
 	char *line;
 	size_t len;
 	int read;
-	char str[6] = {"exit"};
+	char**argv = NULL;
 
 	while (1)
 	{
 		if(isatty(STDIN_FILENO))
 			write(1, "(mcpshell) ", 10);
 		read = getline(&line, &len, stdin);
-	if ((exit_hash(eof, argv)) == 0)
+	if ((exit_hash(endof, argv)) == 0)
 	{
 		break;
 	}
