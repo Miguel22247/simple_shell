141a473024c421125ddef79d402c3d631e6db1ea
Miguel22247 <minipachru@gmail.com>
2021-04-08 15:10:15 -0300

    added new files

---
+++ clean_files/shell.h
@@ -0,0 +1,18 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+/* Libraries */
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
+/* Functions */
+
+/* main function */
+int shell(void);
+
+#endif
