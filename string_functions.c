c7ae60aae5b2eb8bba2fe0ed5a3f9560e61ecb3c
claraberriel <berrielclara@gmail.com>
2021-04-08 15:52:16 -0300

    str funs

--- string_functions.c
+++ string_functions.c
@@ -95,26 +95,3 @@ char *_strcpy(char *dest, char *src)
 	}
 	return (dest);
 }
-
-/**
- * _strdup - String dup.
- * @str: string
- * Return: a pointer to a newly alloc space in memory, containing a copy of str
- */
-
-char *_strdup(char *str)
-{
-	int i;
-	char *p;
-
-	if (str == NULL)
-		return (NULL);
-	p = malloc(sizeof(char) * (_strlen(str) + 1));
-	if (p == NULL)
-		return (NULL);
-	for (i = 0 ; i < (_strlen(str) + 1) ; i++)
-	{
-		p[i] = str[i];
-	}
-	return (p);
-}
