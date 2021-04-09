1ba3944a433fbc02895038c59a052994a835e48b
Miguel22247 <minipachru@gmail.com>
2021-04-09 19:07:24 -0300

    fixed betty style

--- shell.c
+++ shell.c
@@ -13,24 +13,24 @@ int main(int argc, char *argv[])
 	ssize_t read;
 
 	while (1)
-	{
+		{
 			if (isatty(STDIN_FILENO) == 1)
-				write(1,"(mcpshell)", 10);
+				write(1, "(mcpshell)", 10);
 			read = getline(&line, &len, stdin);
-			
+
 			if (read == -1) /* EOF */
 			{
 				if (isatty(STDIN_FILENO) == -1)
-						write(1,"\n", 1);
+						write(1, "\n", 1);
 				break;
 				
 			}
-			if(line[read - 1] == '\n')
+			if (line[read - 1] == '\n')
 					line[read - 1] = '\0';
 			if (*line == '\0')
 				continue;
 	}
-	free (line);
+	free(line);
 	line = NULL;
 	return (0);
 }
