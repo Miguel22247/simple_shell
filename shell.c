98ceede35d120c03e45708455244d86622805cd8
Miguel22247 <minipachru@gmail.com>
2021-04-09 18:46:19 -0300

    trying to test things

--- shell.c
+++ shell.c
@@ -9,22 +9,26 @@
 int main(int argc, char *argv[])
 {
 	char *line;
-	/* char **argv; */
 	size_t len;
 	ssize_t read;
 
 	while (1)
 	{
-		if (isatty(STDIN_FILENO) == 1)
-			write(1,"(mcpshell)", 10);
-		read = getline(&line, &len, stdin);
-		if (read == -1)
-		{
-			if (isatty(STDIN_FILENO) == -1)
-					write(1,"\n", 1);
-			break;
+			if (isatty(STDIN_FILENO) == 1)
+				write(1,"(mcpshell)", 10);
+			read = getline(&line, &len, stdin);
 			
-		}
+			if (read == -1)
+			{
+				if (isatty(STDIN_FILENO) == -1)
+						write(1,"\n", 1);
+				break;
+				
+			}
+			if(line[read - 1] == '\n')
+					line[read -1] = '\0';
+			if (*line == '\0')
+				continue;
 	}
 	free (line);
 	line = NULL;
