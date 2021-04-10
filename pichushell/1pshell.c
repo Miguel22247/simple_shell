dd03ff04deda4df500efd2fde9bf8b172631426c
Miguel22247 <minipachru@gmail.com>
2021-04-10 19:30:20 -0300

    testing things

--- pichushell/1pshell.c
+++ pichushell/1pshell.c
@@ -16,7 +16,7 @@ int main(int argc, char *argv[])
 	{
 		if (isatty(STDIN_FILENO) == 1)
 		{
-			write(1, "$ ", 2);
+			write(1, "(mpcshell) ", 10);
 		}
 		read = getline(&buffer, &length, stdin);
 		if (read == -1)
@@ -25,13 +25,15 @@ int main(int argc, char *argv[])
 					write(1, "\n", 1);
 			break;
 		}
-		/**if (read == 0)
-		{
-			free(buffer);
-			exit(0);
-		}
-	*/
+		
+		if (buffer[read - 1] == '\n')
+				buffer[read - 1] = '\0';
+		if (*buffer == '\0')
+				continue;
+		if (commandread(buffer, read) == 2)
+				break;
 	}
 	free(buffer);
+	buffer = NULL;
 	return (0);
 }
