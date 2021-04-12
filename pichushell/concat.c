924b849afee8c6669fc413f64acd2effa4b91fee
Miguel22247 <minipachru@gmail.com>
2021-04-12 15:50:28 -0300

    modified the code of strcat

--- pichushell/concat.c
+++ pichushell/concat.c
@@ -1,38 +1,35 @@
-#include "holberton.h"
-
-unsigned int _strlen(char *s);
-
-/**
- * string_nconcat - concatenates two strings,
- * the entire first string and n chars from the second string.
- * @s1: first string .
- * @s2: second string.
- * @n: numbers of chars to concatinate from the second string.
- * Return: pointer to the new string.
- **/
+#include"header.h"
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int pos_s1, pos_s2, pos_concat;
-	char *str_concat;
+	unsigned int len1, len2, i;
+	char *ptr, *ret;
 
-	pos_s1 = 0;
-	if (s1)
-		while (s1[pos_s1])
-			pos_s1++;
-	pos_s2 = 0;
-	if (s2)
-		while (s2[pos_s2])
-			pos_s2++;
-	if (pos_s2 > n)
-		pos_s2 = n;
-	str_concat = malloc(sizeof(char) * (pos_s1 + pos_s2 + 1));
-	if (!str_concat)
-		return (NULL);
-	for (pos_concat = 0; pos_concat < pos_s1; pos_concat++)
-		str_concat[pos_concat] = s1[pos_concat];
-	for (pos_concat = 0; pos_concat < pos_s2; pos_concat++)
-		str_concat[pos_concat + pos_s1] = s2[pos_concat];
-	str_concat[pos_s1 + pos_s2] = '\0';
-	return (str_concat);
+	if (!s1)
+		s1 = "";
+	if (!s2)
+		s2 = "";
+
+	for (len1 = 0, ptr = s1; *ptr; ptr++)
+		len1++;
+	for (len2 = 0, ptr = s2; *ptr; ptr++)
+		len2++;
+	if (n > len2)
+		n = len2;
+
+	ret = malloc((len1 + n + 1) * sizeof(char));
+	if (!ret)
+		return (0);
+
+	ptr = ret;
+	while (*s1)
+		*ptr++ = *s1++;
+	i = 0;
+	while (i < n)
+	{
+		*ptr++ = s2[i++];
+	}
+	*ptr = 0;
+
+	return (ret);
 }
