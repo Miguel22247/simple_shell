814fb5cbc768684242da7ee97ad27a2531edd181
Miguel22247 <minipachru@gmail.com>
2021-04-13 10:59:13 -0300

    added if conditions and a new function

--- clara_shell/shell.c
+++ clara_shell/shell.c
@@ -130,13 +130,49 @@ int shell_loop(void)
 *
 *
 */
-int execute(char **args __attribute__((unused)))
+int execute(char *args[])
 {
+	char *executable_path = NULL;
+	char *command = NULL;
 	pid_t pid;
-	/*int status;*/
+	int status;
 
+	command = args[];
+	executable_path = /*path_finder*/;
+	
+	if (executable_path == NULL)
+	{
+		not_found(command);
+		return (3);
+	}
+	
 	pid = fork();
 
+	if (pid < 0)
+	{
+		perror("Error: ");
+		return (-1);
+	}
+	if (pid > 0)
+	{
+		wait(&status);
+	}
+
+	if (pid == 0)
+	{
+		execve();
+		perror("Error: ");
+		exit(1);
+	}
+
+	free(executable_path);
 	return (0);
 }
 
+int not_found(char *command)
+{
+	write(2, name, strlen(name)); /**no se que poner aca*/
+	write(2, ": 1: ", 5);
+	write(2, command, strlen(command));
+	write(2, ": not found\n", 12);
+}
