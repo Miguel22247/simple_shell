361b453b1070a38ae31153f1add80467b55e4a85
pichu185 <pichu_otegui@hotmail.com>
2021-04-13 18:35:55 +0000

    pichu

---
+++ test1304/aux_funs.c
@@ -0,0 +1,93 @@
+#include "shell.h"
+
+/**
+ * _puts - outputs a line of stream
+ * @str: inherited from main
+ * Return: 0
+ */
+
+void _puts(char *str)
+{
+int i;
+for (i = 0; str[i] != '\0'; i++)
+{_putchar(str[i]);
+}
+_putchar('\n');
+}
+
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+
+/**
+ * _strdup - duplicates a string into newly allocated array
+ *
+ * @str: string to duplicate
+ *
+ * Return: pointer to new string
+ */
+char *_strdup(char *str)
+{
+	int size = 0;
+	char *ptr, *ret;
+
+	if (!str)
+		return (NULL);
+
+	ptr = str;
+	while (*ptr++)
+		size++;
+
+	ret = malloc(size + 1);
+	if (!ret)
+		return (NULL);
+
+	ptr = ret;
+	while (*str)
+		*ptr++ = *str++;
+
+	*ptr = 0;
+	ret[size] = '\0';
+	return (ret);
+}
+
+/**
+ * _strcmp - a function that compares two strings
+ * @s1: input one
+ * @s2: input two
+ * Return: Always 0 (Success)
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+	}
+	return (0);
+}
+
+void free_tokens(char **tokens)
+{
+	int i = 0;
+
+	while (tokens[i] != NULL)
+	{
+		free(tokens[i]);
+		i++;
+	}
+
+	free(tokens);
+}
