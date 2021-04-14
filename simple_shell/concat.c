28099b50cde0782f6596ec4c0479ab426113be48
pichu185 <pichu_otegui@hotmail.com>
2021-04-14 20:08:58 +0000

    simple_shell/

---
+++ simple_shell/concat.c
@@ -0,0 +1,41 @@
+#include "header.h"
+/**
+ * string_nconcat - a function that concat two strings
+ * @s1: string one
+ * @s2: string two
+ * @n: an unsigned int
+ * Return: two strings concatenated
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int len1, len2, i;
+	char *ptr, *ret;
+
+	if (!s1)
+		s1 = "";
+	if (!s2)
+		s2 = "";
+
+	for (len1 = 0, ptr = s1; *ptr; ptr++)
+		len1++;
+	for (len2 = 0, ptr = s2; *ptr; ptr++)
+		len2++;
+	if (n > len2)
+		n = len2;
+
+	ret = malloc((len1 + n + 1) * sizeof(char));
+	if (!ret)
+		return (0);
+
+	ptr = ret;
+	while (*s1)
+		*ptr++ = *s1++;
+	i = 0;
+	while (i < n)
+	{
+		*ptr++ = s2[i++];
+	}
+	*ptr = 0;
+
+	return (ret);
+}
