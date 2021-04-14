7c8ac4de1098b1dcd78b063e8386e4d9db05b8b6
Miguel22247 <minipachru@gmail.com>
2021-04-14 10:22:22 -0300

    testing

--- shell.c
+++ shell.c
@@ -121,8 +121,17 @@ int shell_loop(void)
 	/* compare to "env" command             ???????? */
 	if (!_strcmp(tokens[0], env))
 	{
+		int i, len = 0;
+		char **env;
+		
+		for (i = 0; env[i] != NULL; i++)
+		{
+			len = _strlen(env[i]);
+			write(1, env[i], len);
+			write(1, "\n", 1);
+		}
 		free_tokens(tokens);
-		exit(0);
+		return(0);
 	}
 
 
